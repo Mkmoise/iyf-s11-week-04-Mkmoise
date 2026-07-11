c2eaa8121db01a8b385db49fd84b68d42a9d3729
Mkmoise <141993247+Mkmoise@users.noreply.github.com>
2026-07-11 21:46:57 +0300

    Add variable declarations and console logs

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// Task 7.1 Variables
+let myName = "Moise Nyarugabo";
+let myAge = 23;
+let isStudent = true;
+const favoriteColors = ["Blue", "Black", "White"];
+const today = new Date();
+
+console.log("Name:", myName);
+console.log("Age:", myAge);
+console.log("Student:", isStudent);
+console.log("Favorite Colors:", favoriteColors);
+console.log("Today's Date:", today);
+
+// typeof examples
+
+console.log(typeof myName);
+console.log(typeof myAge);
+console.log(typeof isStudent);
+console.log(typeof favoriteColors);
